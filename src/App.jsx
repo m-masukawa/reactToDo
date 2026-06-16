2f66482da23b8fda349f28199bca27e89422a51a
m.masukawa <m.masukawa@core-tech.jp>
2026-06-16 14:26:17 +0900

    feat: 共通：初期環境構築

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+// src/App.jsx
+function App() {
+  return (
+    <div className="app" style={{ padding: "20px", maxWidth: "400px", margin: "0 auto" }}>
+      <h1>Todoアプリ</h1>
+    </div>
+  );
+}
+export default App;
